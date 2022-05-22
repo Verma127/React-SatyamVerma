762f7aeb1c3f2bbee0528881d4ee2e9960d86181
Satyam Verma <satyam.verma@kgpian.iitkgp.ac.in>
2022-05-23 00:03:05 +0530

    Assignment 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {
     constructor(props) {
@@ -18,28 +19,7 @@ class Menu extends Component {
     onDishSelect(dish) {
       this.setState({ selectedDish: dish});
     }
-
-    renderDish(dish) {
-      if(dish != null) {
-        return(
-          <div className="col-12 col-md-5 m-1">
-            <Card>
-              <CardImg width="100%" src={dish.image} alt={dish.name} />
-              <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-              </CardBody>
-            </Card>
-          </div>
-        );
-      }
-      else {
-        return(
-          <div></div>
-        );
-      }
-    }
-
+  
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
@@ -61,9 +41,7 @@ class Menu extends Component {
             <div className="row">
               {menu}
             </div>
-            <div className="row">
-              {this.renderDish(this.state.selectedDish)}
-            </div>
+            <Dishdetail dish={this.state.selectedDish} />
           </div>
         );
     }
